c4bae536b15416cbb7710b36ed9a299e5005d740
root <root@vps2.(none)>
2014-04-06 13:30:11 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c1991adca9622e33f3ce9a7a42fafe7ff91f7da738c3f72993c2cc179cc5fed157db7ebe48f4233f5c3fc7caeda0a0a2739ac691a2f0b72c41f73ec52918e0b4'
